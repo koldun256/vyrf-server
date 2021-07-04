7bf771155c45b28993e856d045495e8e0016470d
koldun <egor.sharypov2005@gmail.com>
2021-07-04 20:54:13 +0000

    player creation on request

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -2,6 +2,7 @@ import { Socket } from "socket.io";
 
 export class Player {
   constructor(
-    private socket: Socket
+    private socket: Socket,
+    private room: Room
   ){}
 }
--- src/user/User.ts
+++ src/user/User.ts
@@ -1,7 +1,15 @@
 import { Socket } from "socket.io"
+import { Player } from "../objects/Player"
+import { RoomManager } from "../room/RoomManager"
 
 export class User {
   constructor(
-    private socket: Socket
-  ){}
+    private socket: Socket,
+    roomManager: RoomManager
+  ){
+    socket.on('room_enter', () => {
+      const room: Room = roomManager.getWaiting()
+      new Player(socket, room)
+    })
+  }
 }
