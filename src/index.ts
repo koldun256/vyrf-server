e6432c4af1d7d1b561b95d0c6531d1290dda5ff2
koldun <egor.sharypov2005@gmail.com>
2021-07-04 20:48:40 +0000

    user creation on connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Server } from 'socket.io'
+import { User } from './user/User'
 
 const io = new Server()
-io.on('connection', socket => console.log('connection'))
+io.on('connection', socket => new User(socket))
