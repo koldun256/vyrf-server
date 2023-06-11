c33b838479bb130a5dfbb1d5538358d7d1615ed2
koldun <egor.sharypov2005@gmail.com>
2023-06-11 23:21:48 +0700

    registering clients

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use std::net::{UdpSocket, SocketAddr};
+use std::{net::{UdpSocket, SocketAddr}, collections::HashSet};
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:8080")?;
     let mut buf = [0; 10];
     let mut count: i32 = 0;
+    let mut clients: HashSet<String> = HashSet::new();
 
     loop {
         let (amt, src) = socket.recv_from(&mut buf)?;
         let msg = &buf[..amt];
+        clients.insert(src.to_string());
+
         count = count + msg[0] as i32;
 
-        socket.send_to(&count.to_be_bytes(), &src)?;
+        for client in &clients {
+            socket.send_to(&count.to_be_bytes(), client).expect("cannot send msg");
+        }
     }
 }
