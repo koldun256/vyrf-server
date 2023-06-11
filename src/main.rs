149b68c350c9300eb2966a14438985acb982d3e7
koldun <egor.sharypov2005@gmail.com>
2023-06-11 18:24:59 +0700

    test udp increment server

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use std::net::UdpSocket;
-
+use std::net::{UdpSocket, SocketAddr};
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:8080")?;
-    let mut buf = [0; 1024];
+    let mut buf = [0; 10];
+    let mut count: i32 = 0;
 
     loop {
         let (amt, src) = socket.recv_from(&mut buf)?;
-        let reply = &buf[..amt];
+        let msg = &buf[..amt];
+        count = count + msg[0] as i32;
 
-        for byte in reply {
-            print!("{} ", byte);
-        }
-        println!();
-
-        socket.send_to(&[0x01, 0x02, 0x03], &src)?;
+        socket.send_to(&count.to_be_bytes(), &src)?;
     }
 }
