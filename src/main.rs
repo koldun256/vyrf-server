9d761a94fc27e59d03296d2098416108664ec8ea
koldun <egor.sharypov2005@gmail.com>
2023-06-12 22:30:49 +0700

    refactoring of udp_server

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,12 @@ use udp_server::{ClientMsg, ServerMsg};
 mod udp_server;
 fn main() -> std::io::Result<()> {
     let (udp_tx, udp_rx) = udp_server::connect();
-    let mut count: i32 = 0;
     let mut clients: HashSet<String> = HashSet::new();
 
     while let Ok((client, msg)) = udp_rx.recv() {
-        clients.insert(client);
-
         match msg {
-            ClientMsg::Increment => count += 1
-        }
-
-        for client in &clients {
-            udp_tx.send((client.to_string(), ServerMsg::Val(count)));
-        }
+            ClientMsg::Register => clients.insert(client)
+        };
     }
 
     Ok(())
--- src/udp_server.rs
+++ src/udp_server.rs
@@ -2,37 +2,43 @@ use std::sync::mpsc::{Receiver, Sender, channel};
 use std::net::UdpSocket;
 use std::thread;
 
-pub enum ServerMsg {
-    Val(i32)
+enum Error {
+    InvalidMessage
 }
+pub enum ServerMsg { }
 
 pub enum ClientMsg {
-    Increment
+    Register
 }
 
-fn recv_msg(buf: &[u8; 4]) -> ClientMsg {
-    ClientMsg::Increment
+fn parse_msg(buf: &[u8; 10]) -> Result<ClientMsg, Error> {
+    if buf == &[0u8; 10] {
+        Ok(ClientMsg::Register)
+    } else {
+        Err(Error::InvalidMessage)
+    }
 }
 
-fn send_msg(msg: ServerMsg, socket: &UdpSocket, addr: String) {
-    match msg {
-        ServerMsg::Val(val) => socket.send_to(&val.to_be_bytes(), addr)
-    };
-}
+fn gen_payload(msg: ServerMsg) -> [u8; 10] { [0; 10] }
 
 pub fn connect() -> (Sender<(String, ServerMsg)>, Receiver<(String, ClientMsg)>) {
     let (tx1, rx1) = channel(); // from client to server
     let (tx2, rx2) = channel(); // from server to client
     let socket1 = UdpSocket::bind("127.0.0.1:8080").expect("cannot open udp socket");
     let socket2 = socket1.try_clone().unwrap();
-    thread::spawn(move || while let Ok((addr, msg)) = rx2.recv() {
-        send_msg(msg, &socket2, addr);
-    });
     thread::spawn(move || {
-        let mut answer = [0; 4];
-        while let Ok((_, client)) = socket1.recv_from(&mut answer) {
-            tx1.send((client.to_string(), recv_msg(&answer)));
+        let mut payload = [0; 10];
+        while let Ok((_, client)) = socket1.recv_from(&mut payload) {
+            match parse_msg(&payload) {
+                Ok(msg) => tx1.send((client.to_string(), msg)).unwrap(),
+                Err(Error::InvalidMessage) => {
+                    println!("invalid msg {:?} from client", &payload);
+                }
+            }
         };
+    });
+    thread::spawn(move || while let Ok((addr, msg)) = rx2.recv() {
+        socket2.send_to(&gen_payload(msg), addr).unwrap();
     });
     (tx2, rx1)
 }
