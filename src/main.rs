2d72af437509c1b4e7fca9f40d69cac2ecbf4118
koldun256 <eg.sharypov@ya.ru>
2023-06-13 14:54:31 +0700

    test commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod game;
 fn main() {
     let (udp_tx, udp_rx) = udp_server::connect();
     let game_tx = Game::launch(udp_tx.clone());
-
+    println!("test commit");
     while let Ok((addr, msg)) = udp_rx.recv() {
         game_tx.send((addr, msg)).expect("game crashed");
     }
